756207585ed3cdbafcc2acab2d69a60ea1b02e96
unknown <iscoramdadane@gmail.com>
2022-08-23 22:00:43 +0000

    version final tp1

---
+++ index.js
@@ -0,0 +1,16 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const cercles = document.querySelectorAll(".cercle");
+
+cercles.forEach((cercle) => {
+  window.addEventListener("mousemove", (e) => {
+    cercle.style.left = e.pageX + "px";
+    cercle.style.top = e.pageY + "px";
+  });
+});
